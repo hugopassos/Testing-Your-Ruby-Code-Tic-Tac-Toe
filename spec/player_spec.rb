fc4cf63235ff1043515510c86e2edc06ad4d645d
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2019-09-26 12:08:27 -0400

    fixed stickler errors

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,11 +5,11 @@
 require_relative '../lib/player.rb'
 
 RSpec.describe Player do
-  let(:name) { "Hugo" }
-  let(:weapon) { "X" }
+  let(:name) { 'Hugo' }
+  let(:weapon) { 'X' }
 
   describe '#initialize' do
-    player = Player.new("Hugo", "X")
+    player = Player.new('Hugo', 'X')
 
     it 'sets up the player name' do
       expect(player.name).to eql(name)
